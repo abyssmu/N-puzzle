0299f9d68f2d367abb80c723e705f39b33dc0d37
abyssmu <abyssmu@hotmail.com>
2018-10-21 22:41:43 -0500

    Rearranged functions into and within Npuzzle namespace. Fixed solvable(). AI now solves all possible board configurations and rejects all non-solvable ones.

--- n-Puzzle/Main.cpp
+++ n-Puzzle/Main.cpp
@@ -1,42 +1,135 @@
-#include "Functions.h"
+#include "Npuzzle.h"
+
+void setMinMax(
+	std::pair<int, double>& p,
+	std::pair<int, double>& min,
+	std::pair<int, double>& max);
+void solveWatch(std::pair<bool, int> info);
+std::pair<bool, int> userInput();
 
 int main()
 {
-	auto start = std::chrono::system_clock::time_point();
-	auto end = std::chrono::system_clock::time_point();
+	auto info = userInput();
 
-	auto b = Npuzzle::Board();
+	if (info.first)
+	{
+		auto p = Npuzzle::solve(info.first, info.second);
 
-	//Open list contains all unexplored nodes, sorted by heuristic value
-	Npuzzle::set open;
+		std::cout << p.first << " steps in\t" << p.second << "s." << std::endl;
+	}
+	else
+	{
+		solveWatch(info);
+	}
 	
-	//Closed list contains all explored nodes, with values set to encoded parent board
-	Npuzzle::map closed;
-
-	auto n = 4;
-
-	//std::cout << "Input size of board: " << std::endl;
-	//std::cin >> n;
-
-	start = std::chrono::system_clock::now();
-	
-	solve(b, open, closed, n);
-	
-	end = std::chrono::system_clock::now();
-
-	auto t = std::chrono::duration<double>();
-	t = end - start;
-
-	auto steps = print(Npuzzle::encode(b, n), closed, n);
-
-	std::cout << std::endl;
-	std::cout << std::fixed;
-	std::cout << std::setprecision(5);
-	std::cout << steps << " steps in " << t.count() << " secs.";
-
-	cleanup(open, closed);
-
+	std::cin.get();
 	std::cin.get();
 
 	return 0;
+}
+
+void setMinMax(
+	std::pair<int, double>& p,
+	std::pair<int, double>& min,
+	std::pair<int, double>& max)
+{
+	if (p.first < min.first)
+	{
+		min.first = p.first;
+	}
+
+	if (p.second < min.second)
+	{
+		min.second = p.second;
+	}
+
+	if (p.first > max.first)
+	{
+		max.first = p.first;
+	}
+
+	if (p.second > max.second)
+	{
+		max.second = p.second;
+	}
+}
+
+void solveWatch(std::pair<bool, int> info)
+{
+	auto trials = 0;
+	auto totalSteps = 0;
+	auto totalT = 0.0;
+	auto min = std::pair<int, double>(INT_MAX, INT_MAX);
+	auto max = std::pair<int, double>(0, 0);
+
+	system("CLS");
+
+	std::cout << "How many trials? ";
+	std::cin >> trials;
+
+	system("CLS");
+
+	for (auto i = 0; i < trials; ++i)
+	{
+		auto p = Npuzzle::solve(info.first, info.second);
+
+		std::cout << p.first << " steps in\t" << p.second << "s." << std::endl;
+
+		setMinMax(p, min, max);
+
+		totalSteps += p.first;
+		totalT += p.second;
+	}
+
+	std::cout << "Average steps: " << totalSteps / trials << std::endl;
+	std::cout << "Average time: " << totalT / trials << std::endl;
+	std::cout << "Min steps: " << min.first << std::endl;
+	std::cout << "Min time: " << min.second << std::endl;
+	std::cout << "Max steps: " << max.first << std::endl;
+	std::cout << "Max time: " << max.second << std::endl;
+}
+
+std::pair<bool, int> userInput()
+{
+	auto n = 0;
+
+	do
+	{
+		if (n > 4)
+		{
+			system("CLS");
+
+			std::cout << "Board size cannot be greater than 4." << std::endl << "This functionality has not been added yet." << std::endl;
+		}
+
+		std::cout << "Input size of board: ";
+		std::cin >> n;
+	} while (n > 4);
+
+	system("CLS");
+
+	auto solve = false;
+	auto go = true;
+
+	while (go)
+	{
+		auto input = ' ';
+
+		std::cout << "Do you want to watch the solve (y/n)? ";
+		std::cin >> input;
+
+		if (input == 'y')
+		{
+			solve = true;
+			go = false;
+		}
+		else if (input == 'n')
+		{
+			solve = go = false;
+		}
+	}
+
+	system("CLS");
+
+	return { solve, n };
 }
--- n-Puzzle/Npuzzle.h
+++ n-Puzzle/Npuzzle.h
@@ -1,16 +1,178 @@
-#include <assert.h>
-#include <cstdint>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <unordered_map>
-#include <vector>
+#include "Headers.h"
 
 namespace Npuzzle
 {
-	using Board = std::vector<int>;
 	using i64 = std::uint_fast64_t;
+
+	namespace Boards
+	{
+		using Board = std::vector<int>;
+
+		int linear(
+			const Board& b,
+			const int& n)
+		{
+			auto count = 0;
+
+			Board inCol(n * n), inRow(n * n);
+
+			for (auto y = 0; y < n; ++y)
+			{
+				for (auto x = 0; x < n; ++x)
+				{
+					auto i = y * n + x;
+
+					if (b[i] == 0)
+					{
+						continue;
+					}
+
+					auto bX = 0;
+					auto bY = 0;
+
+					if (b[i] % n == 0)
+					{
+						bX = n - 1;
+						bY = b[i] / n - 1;
+					}
+					else
+					{
+						bX = b[i] % n - 1;
+						bY = b[i] / n;
+					}
+
+					inCol[i] = (bX == x);
+					inRow[i] = (bY == y);
+				}
+			}
+
+			for (auto y = 0; y < n; ++y)
+			{
+				for (auto x = 0; x < n; ++x)
+				{
+					auto i = y * n + x;
+
+					if (b[i] == 0)
+					{
+						continue;
+					}
+
+					if (inCol[i])
+					{
+						for (auto z = y; z < n; ++z)
+						{
+							auto j = z * n + x;
+
+							if (b[j] == 0)
+							{
+								continue;
+							}
+
+							if (inCol[j])
+							{
+								if ((b[j] < b[i]) && ((abs(b[j] - b[i]) % n) == 0))
+								{
+									++count;
+								}
+							}
+						}
+					}
+
+					if (inRow[i])
+					{
+						auto bI = b[i];
+
+						for (auto z = x + 1; z < n; ++z)
+						{
+							auto j = y * n + z;
+							auto bJ = b[j];
+
+							if (b[j] == 0)
+							{
+								continue;
+							}
+
+							if (inRow[j])
+							{
+								if ((bJ < bI) && (0 <= (bI - bJ)) && ((bI - bJ) < n))
+								{
+									++count;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			return 2 * count;
+		}
+
+		int manhattan(
+			const Board& b,
+			const int& n)
+		{
+			auto m = 0;
+
+			Board solution(n * n);
+			std::iota(solution.begin(), solution.end(), 1);
+
+			solution[n * n - 1] = 0;
+
+			//Calculate manhattan distance for each value
+			for (auto i = 0; i < n * n; ++i)
+			{
+				if (b[i] != solution[i])
+				{
+					auto bX = 0;
+					auto bY = 0;
+					auto x = 0;
+					auto y = 0;
+
+					if (b[i] == 0)
+					{
+						++i;
+					}
+
+					//Calculate goal pos
+					if ((b[i] % n) == 0)
+					{
+						bX = n - 1;
+						bY = b[i] / n - 1;
+					}
+					else
+					{
+						bX = b[i] % n - 1;
+						bY = b[i] / n;
+					}
+
+					//Calculate the current pos
+					auto val = i + 1;
+
+					if ((val % n) == 0)
+					{
+						x = n - 1;
+						y = val / n - 1;
+					}
+					else
+					{
+						x = val % n - 1;
+						y = val / n;
+					}
+
+					m += abs(bX - x) + abs(bY - y);
+				}
+			}
+
+			return m;
+		}
+
+		int heuristic(
+			const Board& b,
+			const int& n)
+		{
+			return manhattan(b, n) + linear(b, n);
+		}
+	}
 
 	namespace Structures
 	{
@@ -22,7 +184,7 @@ namespace Npuzzle
 		struct Container
 		{
 			int heuristic;
-			Board board;
+			Boards::Board board;
 		};
 
 		struct LessThanByHeur
@@ -41,8 +203,8 @@ namespace Npuzzle
 	using map = std::unordered_map<i64, i64>;
 
 	Structures::Point findZero(
-		const Board b,
-		const int n)
+		const Boards::Board& b,
+		const int& n)
 	{
 		for (auto i = 0; i < n * n; ++i)
 		{
@@ -55,10 +217,14 @@ namespace Npuzzle
 		return { -1, -1 };
 	}
 
-	//Count inversions in board
+	bool isEven(const int& i)
+	{
+		return (i % 2 == 0);
+	}
+
 	int inversions(
-		const Board b,
-		const int n)
+		const Boards::Board& b,
+		const int& n)
 	{
 		auto count = 0;
 
@@ -66,14 +232,12 @@ namespace Npuzzle
 		{
 			for (auto j = i + 1; j < n * n; ++j)
 			{
-				if (b[i] == 0)
+				if ((b[i] == 0) || (b[j] == 0))
 				{
-					if (b[j] < n * n)
-					{
-						++count;
-					}
+					continue;
 				}
-				else if (b[j] < b[i])
+
+				if (b[j] < b[i])
 				{
 					++count;
 				}
@@ -84,58 +248,41 @@ namespace Npuzzle
 	}
 
 	bool solvable(
-		const Board b,
-		const int n)
+		const Boards::Board& b,
+		const int& n)
 	{
-		auto zero = findZero(b, n);
 		auto count = inversions(b, n);
 
-		//If width is odd and count is even
-		if ((n & 1) && !(count & 1))
+		if (!isEven(n))
 		{
-			return true;
-		}
-		//If width is even
-		else
-		{
-			//If zero y pos is odd from bottom, and count is even
-			if (((n - zero.y) & 1) && !(count & 1))
-			{
-				return true;
-			}
-			else if (count & 1)
-			{
-				return true;
-			}
+			return isEven(count);
 		}
 
-		return false;
+		return (isEven(findZero(b, n).y) != isEven(count));
 	}
 
-	Board createBoard(
-		const int n)
+	Boards::Board createBoard(
+		const int& n)
 	{
-		auto b = Board(n * n);
+		auto b = Boards::Board(n * n);
 		auto rng = std::mt19937_64(std::random_device()());
+
+		//Fill vector from 0 to n * n
+		std::iota(b.begin(), b.end(), 0);
 
 		do
 		{
-			//Fill vector from 0 to n * n
-			std::iota(b.begin(), b.end(), 0);
-
-			//Randomize vector
 			std::shuffle(b.begin(), b.end(), rng);
-
 		} while (!solvable(b, n));
 
 		return b;
 	}
 
-	Board decode(
+	Boards::Board decode(
 		i64& code,
-		const int n)
+		const int& n)
 	{
-		static Board b(n * n);
+		static Boards::Board b(n * n);
 
 		for (auto i = (n * n) - 1; i >= 0; --i)
 		{
@@ -155,8 +302,8 @@ namespace Npuzzle
 	}
 
 	i64 encode(
-		const Board b,
-		const int n)
+		const Boards::Board& b,
+		const int& n)
 	{
 		i64 code = 0;
 
@@ -177,179 +324,14 @@ namespace Npuzzle
 		return code;
 	}
 
-	int linear(
-		const Board b,
-		const int n)
-	{
-		auto count = 0;
-
-		Board inCol(n * n), inRow(n * n);
-
-		for (auto y = 0; y < n; ++y)
-		{
-			for (auto x = 0; x < n; ++x)
-			{
-				auto i = y * n + x;
-
-				if (b[i] == 0)
-				{
-					continue;
-				}
-
-				auto bX = 0;
-				auto bY = 0;
-
-				if (b[i] % n == 0)
-				{
-					bX = n - 1;
-					bY = b[i] / n - 1;
-				}
-				else
-				{
-					bX = b[i] % n - 1;
-					bY = b[i] / n;
-				}
-
-				inCol[i] = (bX == x);
-				inRow[i] = (bY == y);
-			}
-		}
-
-		for (auto y = 0; y < n; ++y)
-		{
-			for (auto x = 0; x < n; ++x)
-			{
-				auto i = y * n + x;
-
-				if (b[i] == 0)
-				{
-					continue;
-				}
-
-				if (inCol[i])
-				{
-					for (auto z = y; z < n; ++z)
-					{
-						auto j = z * n + x;
-
-						if (b[j] == 0)
-						{
-							continue;
-						}
-
-						if (inCol[j])
-						{
-							if ((b[j] < b[i]) && ((abs(b[j] - b[i]) % n) == 0))
-							{
-								++count;
-							}
-						}
-					}
-				}
-
-				if (inRow[i])
-				{
-					auto bI = b[i];
-
-					for (auto z = x + 1; z < n; ++z)
-					{
-						auto j = y * n + z;
-						auto bJ = b[j];
-
-						if (b[j] == 0)
-						{
-							continue;
-						}
-
-						if (inRow[j])
-						{
-							if ((bJ < bI) && (0 <= (bI - bJ)) && ((bI - bJ) < n))
-							{
-								++count;
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return 2 * count;
-	}
-
-	int manhattan(
-		const Board b,
-		const int n)
-	{
-		auto m = 0;
-
-		Board solution(n * n);
-		std::iota(solution.begin(), solution.end(), 1);
-
-		solution[n * n - 1] = 0;
-
-		//Calculate manhattan distance for each value
-		for (auto i = 0; i < n * n; ++i)
-		{
-			if (b[i] != solution[i])
-			{
-				auto bX = 0;
-				auto bY = 0;
-				auto x = 0;
-				auto y = 0;
-
-				if (b[i] == 0)
-				{
-					++i;
-				}
-
-				//Calculate goal pos
-				if ((b[i] % n) == 0)
-				{
-					bX = n - 1;
-					bY = b[i] / n - 1;
-				}
-				else
-				{
-					bX = b[i] % n - 1;
-					bY = b[i] / n;
-				}
-
-				//Calculate the current pos
-				auto val = i + 1;
-
-				if ((val % n) == 0)
-				{
-					x = n - 1;
-					y = val / n - 1;
-				}
-				else
-				{
-					x = val % n - 1;
-					y = val / n;
-				}
-
-				m += abs(bX - x) + abs(bY - y);
-			}
-		}
-
-		return m;
-	}
-	
-	int heuristic(
-		const Board b,
-		const int n)
-	{
-		return manhattan(b, n) + linear(b, n);
-	}
-
-	Board swapPos(
-		const Board b,
-		const int n,
-		const Structures::Point zero,
-		const int newPos)
+	Boards::Board swapPos(
+		const Boards::Board& b,
+		const int& n,
+		const Structures::Point& zero,
+		const int& newPos)
 	{
 		auto oldPos = 0;
-		Board move(n * n);
+		Boards::Board move(n * n);
 
 		//Calculate old pos
 		oldPos = zero.x + (zero.y * n);
@@ -367,9 +349,9 @@ namespace Npuzzle
 		return move;
 	}
 
-	Board down(
-		const Board b,
-		const int n)
+	Boards::Board down(
+		const Boards::Board& b,
+		const int& n)
 	{
 		Structures::Point zero = findZero(b, n);
 		auto newPos = zero.y + 1;
@@ -377,16 +359,16 @@ namespace Npuzzle
 		//Check if move is possible
 		if (newPos > (n - 1))
 		{
-			return Board(0);
+			return Boards::Board(0);
 		}
 
 		//Create new board based on newPos
 		return swapPos(b, n, zero, zero.x + (newPos * n));
 	}
 
-	Board left(
-		const Board b,
-		const int n)
+	Boards::Board left(
+		const Boards::Board& b,
+		const int& n)
 	{
 		Structures::Point zero = findZero(b, n);
 		auto newPos = zero.x - 1;
@@ -394,16 +376,16 @@ namespace Npuzzle
 		//Check if move is possible
 		if (newPos < 0)
 		{
-			return Board(0);
+			return Boards::Board(0);
 		}
 
 		//Create new board based on newPos
 		return swapPos(b, n, zero, newPos + (zero.y * n));
 	}
 
-	std::vector<int> right(
-		const Board b,
-		const int n)
+	Boards::Board right(
+		const Boards::Board& b,
+		const int& n)
 	{
 		Structures::Point zero = findZero(b, n);
 		auto newPos = zero.x + 1;
@@ -411,16 +393,16 @@ namespace Npuzzle
 		//Check if move is possible
 		if (newPos > (n - 1))
 		{
-			return Board(0);
+			return Boards::Board(0);
 		}
 
 		//Create new board based on newPos
 		return swapPos(b, n, zero, newPos + (zero.y * n));
 	}
 
-	Board up(
-		const Board b,
-		const int n)
+	Boards::Board up(
+		const Boards::Board& b,
+		const int& n)
 	{
 		Structures::Point zero = findZero(b, n);
 		auto newPos = zero.y - 1;
@@ -428,10 +410,193 @@ namespace Npuzzle
 		//Check if move is possible
 		if (newPos < 0)
 		{
-			return Board(0);
+			return Boards::Board(0);
 		}
 
 		//Create new board based on newPos
 		return swapPos(b, n, zero, zero.x + (newPos * n));
+	}
+
+	bool duplicate(
+		const Boards::Board& b,
+		map& closed,
+		const int& n)
+	{
+		return closed.count(encode(b, n));
+	}
+
+	void addQueue(
+		const Boards::Board& b,
+		const Boards::Board& parent,
+		set& open,
+		map& closed,
+		const int& n)
+	{
+		auto c = new Structures::Container;
+
+		c->board = b;
+		c->heuristic = Boards::heuristic(b, n);
+
+		open.emplace(c);
+
+		closed.insert({ encode(b, n), encode(parent, n) });
+	}
+
+	void addMoves(
+		const Boards::Board& b,
+		set& open,
+		map& closed,
+		const int& n)
+	{
+		auto moves = std::vector<Boards::Board>(4);
+		auto parent = b;
+
+		moves[0] = up(b, n);
+		moves[1] = down(b, n);
+		moves[2] = left(b, n);
+		moves[3] = right(b, n);
+
+		for (auto i = 0; i < 4; ++i)
+		{
+			if (moves[i].size() == (n * n))
+			{
+				if (!duplicate(moves[i], closed, n))
+				{
+					addQueue(moves[i], parent, open, closed, n);
+				}
+			}
+		}
+	}
+
+	void printBoard(
+		const Boards::Board& b,
+		const int& n)
+	{
+		for (auto j = 0; j < n * n; ++j)
+		{
+			std::cout << b[j] << "\t";
+
+			if (j % n == 3)
+			{
+				std::cout << std::endl;
+			}
+		}
+	}
+
+	std::pair<int, double> print(
+		i64 b,
+		map& closed,
+		const double& t,
+		const bool& solve,
+		const int& n)
+	{
+		std::vector<Boards::Board> solution;
+
+		do
+		{
+			auto p = b;
+
+			solution.push_back(decode(b, n));
+
+			b = closed[p];
+		} while (b != 0);
+
+		auto size = int(solution.size() - 1);
+
+		if (solve)
+		{
+			for (auto i = size; i >= 0; --i)
+			{
+				printBoard(solution[i], n);
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(25));
+
+				if (i != 0)
+				{
+					system("CLS");
+				}
+			}
+		}
+
+		return { size + 1, t };
+	}
+
+	void writeBoard(
+		const Boards::Board& b,
+		const int& n)
+	{
+		std::ofstream board("board.csv");
+
+		for (auto i = 0; i < n; ++i)
+		{
+			for (auto j = 0; j < n; ++j)
+			{
+				auto k = i * n + j;
+
+				board << b[k] << ",";
+			}
+
+			board << std::endl;
+		}
+	}
+
+	void cleanup(
+		set& open,
+		map& closed)
+	{
+		while (!open.empty())
+		{
+			delete open.top();
+
+			open.pop();
+		}
+
+		closed.clear();
+	}
+
+	std::pair<int, double> solve(
+		const bool& solve,
+		const int& n)
+	{
+		//Open list contains all unexplored nodes, sorted by heuristic value
+		Npuzzle::set open;
+
+		//Closed list contains all explored nodes, with values set to encoded parent board
+		Npuzzle::map closed;
+
+		auto curr = Npuzzle::createBoard(n);
+		//curr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15 };
+
+		addQueue(curr, Boards::Board(n * n), open, closed, n);
+
+		auto solved = false;
+		auto start = std::chrono::system_clock::now();
+
+		while (!solved)
+		{
+			curr = open.top()->board;
+
+			if (open.top()->heuristic == 0)
+			{
+				solved = true;
+			}
+			else
+			{
+				//writeBoard(curr, n);
+
+				delete open.top();
+				open.pop();
+
+				addMoves(curr, open, closed, n);
+			}
+		}
+
+		auto end = std::chrono::system_clock::now();
+		auto t = std::chrono::duration<double>(end - start);
+		auto results = print(encode(curr, n), closed, t.count(), solve, n);
+
+		cleanup(open, closed);
+
+		return results;
 	}
 }
